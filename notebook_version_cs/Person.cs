64d1faa99d7870bfd010b68920a37c06fdea030b
darkdeaddaset <62422409+darkdeaddaset@users.noreply.github.com>
2021-10-22 23:03:44 +0300

    first_version_cs

---
+++ notebook_version_cs/Person.cs
@@ -0,0 +1,46 @@
+using System;
+
+namespace Notebook
+{
+    public class Person
+    {
+        public int Id { get; set; }
+        public int age { get; set; }
+        public string name { get; set; }
+        public string number { get; set; }
+        public string email { get; set; }
+
+        public Person():this("not number")
+        {
+            //throw new Exception("Нельзя создать пустой контакт\n Необходим хотя бы номер");
+        }
+        public Person(string number):this(number,"no name", 0)
+        {
+
+        }
+        public Person(string number, string name):this(number, name, 0)
+        {
+
+        }
+        public Person(string number, string name, int age) : this(number, name, age, "example@mail.com")
+        {
+
+        }
+        public Person(string number, string name, int age, string email)
+        {
+            this.age = age;
+            this.name = name;
+            this.number = number;
+            this.email = email;
+        }
+
+        public void GetInfo()
+        {
+            Console.WriteLine(age);
+            Console.WriteLine(name);
+            Console.WriteLine(number);
+            Console.WriteLine(email);
+        }
+    }
+}
+
